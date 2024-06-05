08918abef867290b55ffbfe7d48da2c1f6df7a99
Jhonatan Martinez <consejerojhon@gmail.com>
2024-06-04 22:06:05 -0400

    Added iframe example, html5 structure and FlexBox Layout.

---
+++ public/js/styles-with-scripts.js
@@ -0,0 +1,23 @@
+//console.log('Working')
+//alert('Working with alert function.')
+
+let title = document.getElementById('title');
+title.innerHTML = 'New title';
+
+title.style.border = '2px solid gray';
+title.style.borderRadius = '10px';
+
+
+
+const number1 = 52;
+number1 = 5145;
+
+let number2 = 70;
+number2 = 150;
+
+let price1 = 652;
+let price2 = 600;
+
+price1 = price2;
+
+console.log('price1', price1)
